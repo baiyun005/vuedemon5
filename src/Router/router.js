f734c50fd94c1140325fe305b3f003b8aa510a06
sugar1236 <852267032@qq.com>
2019-01-26 15:45:55 +0800

    再次修改router

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from "../views/Index"
-<<<<<<< HEAD
 import Order from '../views/Order'
 import login from "../views/login"
 import changepw from "../views/changepw"
 import Mershopping from "../views/Mershopping"
-=======
 import Position from '../views/Position'
 import Search from '../views/Search'
 
->>>>>>> position
 Vue.use(Router)
 
 
@@ -24,7 +21,7 @@ export default new Router({
     {
       path:"/Index",
       component:Index
-    },
+    },            
     {
       path:"/order",
       component:Order
